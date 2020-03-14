5078418ebdbd489d82fea27c937335cc7b584082
Mahmoud Elnaggar <melnaggar@Netways.com>
2020-03-14 13:00:06 +0300

    upgrade dapper

--- MyAddressBookPlus/Data/ContactRepository.cs
+++ MyAddressBookPlus/Data/ContactRepository.cs
@@ -1,5 +1,4 @@
-﻿using Dapper;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
@@ -20,37 +19,42 @@ namespace MyAddressBookPlus.Data
 
         public int AddContact(Contact contact)
         {
-            var sql = "INSERT INTO dbo.[Contact] ([Name] ,[Email] ,[Phone] ,[Address] ,[PictureName]) VALUES" +
-                "(@Name, @Email, @Phone, @Address, @Picturename); " +
-                "SELECT CAST(SCOPE_IDENTITY() AS INT)";
-            var id = this.db.Query<int>(sql, contact).Single();
-            contact.Id = id;
-            return id;
+            //var sql = "INSERT INTO dbo.[Contact] ([Name] ,[Email] ,[Phone] ,[Address] ,[PictureName]) VALUES" +
+            //    "(@Name, @Email, @Phone, @Address, @Picturename); " +
+            //    "SELECT CAST(SCOPE_IDENTITY() AS INT)";
+            //var id = this.db.Query<int>(sql, contact).Single();
+            //contact.Id = id;
+            //return id;
+            throw new System.Exception();
         }
 
         public bool DeleteContact(int id)
         {
-            var sql = "DELETE FROM dbo.[Contact] WHERE id = @id";
-            var result = db.Execute(sql, new { Id = id });
+            //var sql = "DELETE FROM dbo.[Contact] WHERE id = @id";
+            ////var result = db.Execute(sql, new { Id = id });
 
-            return true;
+            //return true;
+
+            throw new System.Exception();
         }
 
         public Contact GetContact(int id)
         {
-            var sql = "SELECT * FROM dbo.[Contact] WHERE id = @id";
-            var result = db.Query<Contact>(sql, new { Id = id })
-                .SingleOrDefault();
+            //var sql = "SELECT * FROM dbo.[Contact] WHERE id = @id";
+            //var result = db.Query<Contact>(sql, new { Id = id })
+            //    .SingleOrDefault();
 
-            return result;
+            //return result;
+            throw new System.Exception();
         }
 
         public List<Contact> GetContacts()
         {
-            var sql = "SELECT * FROM dbo.[Contact] order by id";
-            var result = db.Query<Contact>(sql).ToList();
+            //var sql = "SELECT * FROM dbo.[Contact] order by id";
+            //var result = db.Query<Contact>(sql).ToList();
 
-            return result;
+            //return result;
+            throw new System.Exception();
         }
     }
 }
